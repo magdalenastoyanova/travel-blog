e30c0468fee557c14c101d2e28301c06cae4dc2a
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2021-04-06 08:46:38 +0300

    Register form error handling

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { Form, Input, Button } from "antd";
+import { useHistory } from 'react-router-dom';
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
 import firebase from "../../firebase/config";
-import style from "./registerModule.css";
+import style from './Register.module.css'
 import photo from '../../images/details.jpg'
 import { toast } from "react-toastify";
 
@@ -15,30 +16,34 @@ const Register = (props) => {
   const [password, setPassword] = useState("");
   const [rePassword, setRePassword] = useState("");
 
-  const signUp = (e) => {
-    e.preventDefault();
-
-    //if (password != rePassword) {
-    // alert("Password must match!");
-    // }
-
-    try {
-      firebase.register(email, password);
-      toast.success(`Successful registration.`, {
-        type: "success",
-        autoClose: 2000,
-        position: "top-center",
-    })
-      history.push("/places");
-    } catch (error) {
-      toast.success(`${error}`, {
+  const signUp = () => {
+    if (password != rePassword) {
+      toast.success(`Password must match!`, {
         type: "error",
         autoClose: 2000,
         position: "top-center",
     })
-      history.push("/register");
-      alert(error);
-    }
+     }else{
+      try {
+        firebase.register(email, password);
+        toast.success(`Successful registration.`, {
+          type: "success",
+          autoClose: 2000,
+          position: "top-center",
+      })
+        history.push("/places")
+  
+      } catch (error) {
+        toast.success(`${error}`, {
+          type: "error",
+          autoClose: 2000,
+          position: "top-center",
+      })
+        history.push("/register");
+      }
+     }
+
+
   };
   const onChangeHandler = (event) => {
     const { name, value } = event.currentTarget;
@@ -57,7 +62,8 @@ const Register = (props) => {
       <div style={{
          backgroundImage: `url(${photo})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover',position: 'relative', width:'100%',height:'100vh',color:'white', opacity: '0.8'
       }}/>
-      <form  className={style.loginBox} >
+       <div className={style.loginBox}>
+      <form  >
       <div className="register-form">
         <h1 className={style.title}>Register</h1>
         <p className="field"  className= {style.placeBox}>
@@ -95,20 +101,21 @@ const Register = (props) => {
             <input className= {style.addInput}
               type="password"
               name="rePassword"
+              value={rePassword} 
               id="rePassword"
+              onChange={(event) => onChangeHandler(event)} 
             />
           </span>
         </p>
-        <input
+        <Button
         className={style.registerBtn}
-          className="button"
           type="submit"
-          className="submit"
           value="Register"
           onClick={signUp}
-        />
+        > Register </Button>
       </div>
       </form>
+      </div>
       <Footer />
     </>
   );
