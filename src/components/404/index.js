f2e233cd4b97bc0e1163348d850bc1e91a2b258f
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2021-04-10 12:53:50 +0300

    404 page update

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,14 +1,23 @@
 import style from "./index.module.css";
 import {Button} from 'antd';
 import { Link } from 'react-router-dom'
-import photo from '../images/404page.png'
+import photo from '../images/error-page.jpg'
 const ErrorPage = () => {
   return (
     <>
     <div style={{
-         backgroundImage: `url(${photo})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover',position: 'relative', width:'70%',height:'80vh',color:'white', marginLeft:'8rem'
+         backgroundImage: `url(${photo})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover', position: 'relative', width:'100%', height:'100vh',color:'white'
       }}/>
-      <Button className={style.backBtn}> <Link to='/'>GO BACK TO HOME PAGE</Link></Button>
+      <Button> <Link to='/' style={{
+        	position: 'absolute',
+          bottom: '5rem',
+          left: '40%',
+          backgroundColor: '#fff',
+          padding: '10px 20px',
+          borderRadius: '10px',
+          textDecoration: 'none',
+          color: '#722364'
+      }}>GO BACK TO HOME PAGE</Link></Button>
     </>
   );
 };
